876916216101e4c4cee487cb124e7b4971945791
Gino Salayo <gino.salayo@gmail.com>
2021-01-22 11:41:46 -0500

    Added color helper to generate colors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Palette from './components/Palette';
 import seedColors from './seeds/seedColors';
+import { generatePalette } from './seeds/colorHelpers';
 import './css/App.css';
 
 function App() {
+    console.log(generatePalette(seedColors[4]));
     return (
         <div className="App">
             <Palette {...seedColors[4]} />
---
+++ src/seeds/colorHelpers.js
@@ -0,0 +1,45 @@
+import chroma from "chroma-js";
+
+const levels = [50];
+for (let i = 100; i <= 900; i += 100) {
+    levels.push(i);
+}
+
+function generatePalette(starterPalette) {
+    let newPalette = { ...starterPalette, colors: {} };
+
+    levels.forEach(level => { newPalette.colors[level] = []; });
+
+    for (let color of starterPalette.colors) {
+        let scale = getScale(color.color, 10).reverse();
+        for (let i in scale) {
+            newPalette.colors[levels[i]].push({
+                name: `${color.name} ${levels[i]}`,
+                id: color.name.toLowerCase().replace(/ /g, "-"),
+                hex: scale[i],
+                rgb: chroma(scale[i]).css(),
+                rgba: chroma(scale[i]).rgba()
+            });
+        }
+    }
+    return newPalette;
+}
+function getRange(hexColor) {
+    const end = "#fff";
+    return [
+        chroma(hexColor)
+            .darken(1.4)
+            .hex(),
+        hexColor,
+        end
+    ];
+}
+
+function getScale(hexColor, numberOfColors) {
+    return chroma
+        .scale(getRange(hexColor))
+        .mode("lab")
+        .colors(numberOfColors);
+}
+
+export { generatePalette };
